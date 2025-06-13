518b543d312794520ccb758562ba1fbf56fc091e
Huascar Cavalcanti Pinheiro <105388129+HuascarPinheiro@users.noreply.github.com>
2025-06-13 00:17:20 -0300

    Adicionando hero

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+const dataDoAniversario = new Date ("Apr 19, 2026 08:00:00");
+const timeStampDoAniversario = dataDoAniversario.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoAniversario - timeStampAtual;
+
+    const diasEmMs = (1000 * 60 * 60 * 24);
+    const horasEmMs = (1000 * 60 * 60);
+    const minutosEmMs = (1000 * 60);
+    
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (diasAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Que pena, você chegou atrasado para o evento.'
+    }
+})
